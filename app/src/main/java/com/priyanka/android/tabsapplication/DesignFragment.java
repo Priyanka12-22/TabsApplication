2bcfa7e22a1a5b0e48b9a80c2798540b34392663
neeraj_soni <neerajsoni461@gmail.com>
2018-02-28 15:34:04 +0530

    Initial commit

---
+++ app/src/main/java/com/priyanka/android/tabsapplication/DesignFragment.java
@@ -0,0 +1,32 @@
+package com.priyanka.android.tabsapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.priyanka.android.tabsapplication.R;
+
+/**
+ * Created by user2 on 2/28/2018.
+ */
+
+public class DesignFragment extends Fragment {
+
+    public DesignFragment()
+    {
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_design, container, false);
+    }
+}
+
